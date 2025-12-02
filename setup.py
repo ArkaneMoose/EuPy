2716bc40d7514726d85f22720a3ec0fc98471a43
j. redhead <jeremyredhead@users.noreply.github.com>
2025-12-01 23:31:00 -0600

    Bump version number to 1.2.1
    I'm calling this a patch release, since the APIs haven't changed.
    
    But one could argue for major or minor, too; maybe it doesn't matter...
    see this post I recently read about the problems with SemVer:
    <https://snarky.ca/why-i-dont-like-semver/>

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='eupy',
-        version='1.2',
-        description='An API to interact with the chatrooms at euphoria.io',
+        version='1.2.1',
+        description='An API to interact with the chatrooms at euphoria.leet.nu',
         author='Justin Chadwell',
         author_email='jedevc@gmail.com',
         url='https://github.com/jedevc/euphoria-python',
